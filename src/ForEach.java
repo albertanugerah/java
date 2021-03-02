b91765ee95949c7b5359f8a4bb510794daa35f1b
Albert Anugerah Putra Siregar <albertanugerah@hotmail.com>
2021-03-02 23:51:31 +0700

    add method and methodparameter

--- src/ForEach.java
+++ src/ForEach.java
@@ -2,7 +2,17 @@ public class ForEach {
 //    foeach digunakan mengakses data di array
     public static void main(String[] args) {
             String[] array = {
-                    "Albert",
+                    "Albert","Anugerah","Putra",
+                    "Albrt","tassya"
             };
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("FOR Each");
+
+        for(var name : array){
+            System.out.println(name);
+        }
     }
 }
---
+++ src/Method.java
@@ -0,0 +1,11 @@
+public class Method {
+    static void sayHelloWorld(){
+        System.out.println("Hello Word");
+        System.out.println("Hello Word 3");
+        System.out.println("Hello Word");
+    }
+
+    public static void main(String[] args) {
+        sayHelloWorld();
+    }
+}
